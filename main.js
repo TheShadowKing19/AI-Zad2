5daed928ace182e9325f09aeae205b4dcd45d8d8
TheShadowKing19 <97754734+TheShadowKing19@users.noreply.github.com>
2022-10-27 18:11:52 +0200

    Koniec zadania

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-function dodajzadanie() {
+function addRow(inputTask, inputDate) {
     const table = document.getElementById("taskTable");
-    var inputTask = document.getElementById("taskInput").value;
-    var inputDate = document.getElementById("dateInput").value;
     const row = document.createElement("tr");
     const cell1 = document.createElement("td");
     const cell1value = document.createTextNode(inputTask);
@@ -18,6 +16,19 @@ function dodajzadanie() {
     row.appendChild(cell2);
     row.appendChild(button);
     table.appendChild(row);
+
+}
+
+function dodajzadanie() {
+
+    var inputTask = document.getElementById("taskInput").value;
+    var inputDate = document.getElementById("dateInput").value;
+
+    var taskObj = {task: inputTask, date: inputDate};
+    tasksArray.push(taskObj);
+    console.log(tasksArray);
+
+    addRow(inputTask, inputDate);
 }
 
 function deleteRow(object) {
@@ -25,42 +36,59 @@ function deleteRow(object) {
     p.parentNode.removeChild(p);
 }
 
-function getData() {
-    var str = localStorage.getItem("localData");
-    if (str != null)
-    {
-        arr = JSON.parse(str);
+function saveData() {
+    localStorage.tasksRecord = JSON.stringify(tasksArray);
+}
+
+var tasksArray = [];
+function init() {
+    if (localStorage.tasksRecord){
+        tasksArray = JSON.parse(localStorage.tasksRecord);
+        for(var i = 0; i < tasksArray.length; i++) {
+            var inputTask = tasksArray[i].task;
+            var inputDate = tasksArray[i].date;
+            addRow(inputTask, inputDate);
+        }
     }
 }
 
-
-
-var dateToday = new Date();
-var month = dateToday.getMonth() + 1;
-var day = dateToday.getDate()
-var year = dateToday.getFullYear();
-if(month < 10)
-    month = '0' + month.toString();
-if(day < 10)
-    day = '0' + day.toString();
-var minDate = year + '-' + month + '-' + day;
-document.getElementById("dateInput").setAttribute("min", minDate);
-var table = document.getElementsByTagName("td");
-// var i;
-// for (i = 0; i < table.length; i++) {
-//     var row = document.getElementsByTagName("tr");
-//     var button = document.createElement("button");
-//     var txt = document.createTextNode("\u00D7");
-//     button.setAttribute("onclick", "deleteRow(this)");
-//     button.className = "closeButton";
-//     button.appendChild(txt);
-//     table[i].appendChild(button);
-// }
-
-var taskTable = document.getElementById("taskTable");
-var j;
-for (j = 0; j < taskTable.length; j++) {
-    console.log(document.getElementsByTagName("td"));
+function szukaj(){
+    var input, filter, table, tr, td, i , txtValue;
+    input = document.getElementById("searcher");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("taskTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++){
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td){
+            txtValue = td.textContent || td.innerText;
+            if(txtValue.toUpperCase().indexOf(filter) > -1){
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 }
+// localStorage.clear();
+function setMinDate(){
+    var dateToday = new Date();
+    var month = dateToday.getMonth() + 1;
+    var day = dateToday.getDate()
+    var year = dateToday.getFullYear();
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+    var minDate = year + '-' + month + '-' + day;
+    document.getElementById("dateInput").setAttribute("min", minDate);
+}
+setMinDate();
+window.addEventListener("beforeunload", saveData);
+
+
+
+
+
 
 
